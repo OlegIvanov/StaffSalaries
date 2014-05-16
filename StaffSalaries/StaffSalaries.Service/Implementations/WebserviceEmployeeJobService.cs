620535a03232362163ed2539617720eea6903ec6
OlegIvanov <iosmsft@outlook.com>
2014-05-16 15:07:52 +0300

    Added a lot of classes

---
+++ StaffSalaries/StaffSalaries.Service/Implementations/WebserviceEmployeeJobService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StaffSalaries.Service.Interfaces;
+
+namespace StaffSalaries.Service.Implementations
+{
+    public class WebserviceEmployeeJobService : IEmployeeJobService
+    {
+    }
+}
---
+++ StaffSalaries/StaffSalaries.Service/Messaging/EmployeeJobService/EmployeeUpdateSalaryResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaffSalaries.Service.Messaging.EmployeeJobService
+{
+    public class EmployeeUpdateSalaryResponse
+    {
+    }
+}
