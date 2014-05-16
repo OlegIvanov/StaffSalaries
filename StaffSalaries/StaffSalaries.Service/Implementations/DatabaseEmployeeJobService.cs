bbaaa4e7f3383bffb25198f7dbdb389628c96087
OlegIvanov <iosmsft@outlook.com>
2014-05-16 15:28:41 +0300

    Service

--- StaffSalaries/StaffSalaries.Service/Implementations/DatabaseEmployeeJobService.cs
+++ StaffSalaries/StaffSalaries.Service/Implementations/DatabaseEmployeeJobService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using StaffSalaries.Service.Interfaces;
+using StaffSalaries.Service.Messaging.EmployeeJobService;
 
 namespace StaffSalaries.Service.Implementations
 {
     public class DatabaseEmployeeJobService : IEmployeeJobService
     {
+        public JobListResponse GetJobList(JobListRequest jobListRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeListResponse GetEmployeeList(EmployeeListRequest employeeListRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeUpdateSalaryResponse EmployeeUpdateSalary(EmployeeUpdateSalaryRequest employeeUpdateSalaryRequest)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- StaffSalaries/StaffSalaries.Service/Interfaces/IEmployeeJobService.cs
+++ StaffSalaries/StaffSalaries.Service/Interfaces/IEmployeeJobService.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StaffSalaries.Service.Messaging.EmployeeJobService;
 
 namespace StaffSalaries.Service.Interfaces
 {
     public interface IEmployeeJobService
     {
+        JobListResponse GetJobList(JobListRequest jobListRequest);
+        EmployeeListResponse GetEmployeeList(EmployeeListRequest employeeListRequest);
+        EmployeeUpdateSalaryResponse EmployeeUpdateSalary(EmployeeUpdateSalaryRequest employeeUpdateSalaryRequest);
     }
 }
