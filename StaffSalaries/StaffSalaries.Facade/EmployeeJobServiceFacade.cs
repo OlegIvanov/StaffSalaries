cffcb7cd5ae5080dec98342c3972038d83a43cda
OlegIvanov <iosmsft@outlook.com>
2014-05-17 12:29:52 +0300

    Facade

---
+++ StaffSalaries/StaffSalaries.Facade/EmployeeJobServiceFacade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StaffSalaries.Service.Interfaces;
+
+namespace StaffSalaries.Facade
+{
+    public class EmployeeJobServiceFacade
+    {
+        private IEmployeeJobService _employeeJobService;
+
+        public EmployeeJobServiceFacade(IEmployeeJobService employeeJobService)
+        {
+            _employeeJobService = employeeJobService;
+        }
+    }
+}
